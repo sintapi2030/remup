c9a0988b81c0096bce5624ce15be279a6d68c295
sintapi2030 <matsushitataku15@gmail.com>
2020-07-16 17:54:39 +0900

    routes_controller_set_commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   get 'comments/index'
       # rootが上?
   root "comments#index"   
+  get 'comments/movies'
+  get 'comments/doramas'
+  get 'comments/musics'
+  get 'comments/games'
+  get 'comments/tools'
+  get 'comments/foods'
+  get 'comments/drinks'
+  get 'comments/refreshs'
+  get 'comments/lifes'
+  get 'comments/studys'
+  get 'comments/sports'
+  get 'comments/freedays'
+  get 'comments/deliverys'
 end
